325bd9fea1ba5042665482aa7aa5a8d02955d867
ele_hopper <elenacerezo83@gmail.com>
2018-05-03 18:36:49 +0200

    second condition and fail test

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@
 *
 *   velocidad de respuesta, acierto o fallo,
 *
-*      Si acierto pregunta en menos de 2 segundos - sumo 2 puntos
+*      +Si acierto pregunta en menos de 2 segundos - sumo 2 puntos
 *          (0 puntos, pregunta correcta, 1 segundo) -> 2 puntos
 *          (1 punto, correcta, 1 segundo) -> 3 puntos
 *      Si fallo pregunta en mas de 10 segundos - resto 2 puntos
-*      Si acierto pregunta entre 2 y 10 segundos - sumo 1 punto
+*      +Si acierto pregunta entre 2 y 10 segundos - sumo 1 punto
             (1 punto, correcta, 5 segundos) -> 2 puntos
-*      Si acierto y tardo mas de 10 segundos - 0 puntos
+*      +Si acierto y tardo mas de 10 segundos - 0 puntos
 *      Si fallo antes de 10 segundos - resto 1 punto
 *      No se puede pasar sin responder
 *      Si en 20 segundos no has respondido , pasa a siguiente pregunta y pierdes 3 punto
@@ -28,6 +28,7 @@ describe('calculo de marcador', function () {
         else if (esCorrecta && tiempo > 10) {
             return puntos;
         }
+
     }
 
     it("suma mas puntos si acierta muy rapido", function () {
@@ -35,5 +36,9 @@ describe('calculo de marcador', function () {
         expect(recalcularMarcador(2, true, 1)).toBe(4);
         expect(recalcularMarcador(2, true, 3)).toBe(3);
         expect(recalcularMarcador(3, true, 11)).toBe(3);
+    });
+    it("resta mas puntos si falla muy lento", function () {
+        expect(recalcularMarcador(3, false, 1)).toBe(2);
+
     });
 });
